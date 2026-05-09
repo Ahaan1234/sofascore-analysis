568dda790fa0189ac613bf482f50f18cfb027937
Ahaan1234 <1ahaanshah@gmail.com>
2026-05-09 19:10:13 +0530

    playing with queries

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,15 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -25,3 +32,9 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
